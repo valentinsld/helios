4fd54774d3c4359162bf345288343eca3aaccd9a
valentin sld <valentin.salaud@gmail.com>
2021-03-22 19:01:51 +0100

    feat(intro) add files for intro

---
+++ src/js/Intro.js
@@ -0,0 +1,5 @@
+export default class Intro{
+  constructor() {
+    console.log('Start intro')
+  }
+}
--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import './style.css'
 
+import Intro from './js/Intro'
 import Game from './js/Game'
 
+new Intro()
 new Game()
