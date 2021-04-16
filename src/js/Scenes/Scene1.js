17afd5b0b805cc0902ad14c1e0cd855682cab934
Valentin S <43878560+valentinsld@users.noreply.github.com>
2021-04-16 16:03:57 +0200

    feat(Scene1) statue en bas

--- src/js/Scenes/Scene1.js
+++ src/js/Scenes/Scene1.js
@@ -287,7 +287,7 @@ export default class Scene1 {
 
     gltf = gltf.scene
     gltf.scale.set(55, 55, 55)
-    gltf.children[0].position.y = -0.17
+    gltf.children[0].position.y = -0.95
     gltf.moreY = Math.PI / 2
 
     gltf.traverse( function(node) {
