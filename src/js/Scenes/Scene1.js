d02f5ddefcbec12a817fd842e425aae538b31087
Valentin S <43878560+valentinsld@users.noreply.github.com>
2021-05-12 12:20:54 +0200

    fix(Scene1) add receiveshadow soleil

--- src/js/Scenes/Scene1.js
+++ src/js/Scenes/Scene1.js
@@ -508,6 +508,7 @@ export default class Scene1 {
       switch (node.name) {
         case 'soleil':
           node.material = this.materialSoleil
+          node.receiveShadow = true
           break;
 
         case 'interieureporte':
