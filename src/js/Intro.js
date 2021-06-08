bc86f95618750ec32c503d7ee0339520683628fd
Vanina <vanina.idiart@mmibordeaux.com>
2021-06-08 18:11:23 +0200

    intro v0

--- src/js/Intro.js
+++ src/js/Intro.js
@@ -5,9 +5,63 @@ export default class Intro{
   }
 }
 
-$('#button').on('click', function() {
-  if ($('#intro').css('opacity') == 0) $('#intro').css('opacity', 1);
-  else $('#intro').css('opacity', 0);
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml12");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+  targets: ".ml12 .letter",
+  translateY: [100, 0],
+  translateZ: 0,
+  opacity: [0, 1],
+  easing: "easeOutExpo",
+  duration: 2000,
+  delay: (el, i) => 2000 + 60 * i,
+});
+
+TweenMax.from(".left", 3, {
+  left: "-50%",
+  ease: Expo.easeInOut,
+  delay: 3.4,
+});
+TweenMax.from(".header h1", 3, {
+  left: "-140%",
+  ease: Expo.easeInOut,
+  delay: 3.4,
+});
+TweenMax.staggerFrom(
+  ".images > div",
+  1,
+  {
+    y: "60",
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 6,
+  },
+  0.2
+);
+
+TweenMax.staggerFrom(
+  ".header > p",
+  1,
+  {
+    y: "60",
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 5.6,
+  },
+  0.2
+);
+
+TweenMax.from(".link", 1, {
+  opacity: 0,
+  ease: SteppedEase.config(1),
+  repeat: -1,
+  repeatDelay: 0.2,
+  delay: 7.8,
 });
 
 //game.sceneManager.next()
