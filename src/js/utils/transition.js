c9169799e45095b1cf46cdf974a06a997a93e7b2
Valentin S <43878560+valentinsld@users.noreply.github.com>
2021-06-10 14:06:57 +0200

    feat(transition)

--- src/js/utils/transition.js
+++ src/js/utils/transition.js
@@ -3,19 +3,17 @@ import gsap from 'gsap'
 const TEXTS = [
   [
     // 'Scene1 -> Scene 2',
-    'Jamais le soleil n’avait rayonné si proche de la Terre ...',
-    'Sans l’intervention de Zeus, ciel, terre et océans se seraient changés en flammes.'
+    'La lumière est revenue.'
   ],
   [
     // 'Scene2 -> Scene 3',
-    'Jamais le soleil n’avait rayonné si proche de la Terre ...',
-    'Sans l’intervention de Zeus, ciel, terre et océans se seraient changés en flammes.'
+    'Dans le ciel, Hélios et son char ont repris leur course.',
+    'Le soleil est redevenu complet ....'
   ]
   ,
   [
     // 'Scene3 -> Scene 4',
-    'Jamais le soleil n’avait rayonné si proche de la Terre ...',
-    'Sans l’intervention de Zeus, ciel, terre et océans se seraient changés en flammes.'
+    'Nous avons réussi notre quête.'
   ]
 ]
 
@@ -25,34 +23,51 @@ class Transition {
     this.elContainerText = this.el.querySelector('.transition__texts')
   }
 
-  async fadeIn (textIndex, duration = 6000, time = 800) {
+  async fadeIn (textIndex, duration = 7000, time = 800) {
     this.timeStartAnimation = new Date()
     this.minDuration = duration
 
     // add text
     this.elContainerText.innerHTML = ''
-    const paragraphes = []
+    this.elContainerText.classList.remove('-solo')
+    this.paragraphes = []
     TEXTS[textIndex].forEach((text) => {
       const paragraphe = document.createElement('p')
       paragraphe.innerHTML = text
 
       this.elContainerText.append(paragraphe)
-      paragraphes.push(paragraphe)
+      this.paragraphes.push(paragraphe)
     })
+    if (TEXTS[textIndex].length === 1) {
+      this.elContainerText.classList.add('-solo')
+    }
 
     // animation
-    gsap.to(
+    const tl = gsap.timeline()
+    tl.to(
       this.el,
       {
         opacity: 1,
+        delay: 1,
         duration: time / 1000
+      }
+    )
+    tl.to(
+      this.paragraphes,
+      {
+        opacity: 1,
+        y: 0,
+        delay: 0.5,
+        duration: 1.5,
+        ease: 'Power4.out',
+        stagger: 1.3
       }
     )
 
     return new Promise(resolve => {
       setTimeout(() => {      
         resolve(true);
-      }, time + 300);
+      }, time + 1000);
     });
   }
 
@@ -65,13 +80,22 @@ class Transition {
 
     // animation
     this.el.style.transitionDuration = ''
-    const tl = gsap.timeline()
+    const tl = gsap.timeline({ delay })
 
+    tl.to(
+      this.paragraphes,
+      {
+        opacity: 0,
+        y: 30,
+        duration: 1,
+        ease: 'Power4.out',
+        stagger: 0.2
+      }
+    )
     tl.to(
       this.el,
       {
         opacity: 0,
-        delay,
         duration: time / 1000,
         onComplete: funcEnd
       }
