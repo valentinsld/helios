ba94d0b2a3581be38e523f634646ddfabcc3f484
Vanina <vanina.idiart@mmibordeaux.com>
2021-06-10 12:55:37 +0200

    v0 intro

--- src/js/Intro.js
+++ src/js/Intro.js
@@ -1,67 +1,177 @@
-export default class Intro{
+export default class Intro {
   constructor(game) {
     console.log('Start intro !')
     console.log(game)
   }
 }
 
-// Wrap every letter in a span
-var textWrapper = document.querySelector(".ml12");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
 
-anime.timeline().add({
-  targets: ".ml12 .letter",
-  translateY: [100, 0],
-  translateZ: 0,
-  opacity: [0, 1],
-  easing: "easeOutExpo",
-  duration: 2000,
-  delay: (el, i) => 2000 + 60 * i,
-});
+// TweenMax.from(".header h1", 3, {
+//   left: "-140%",
+//   ease: Expo.easeInOut,
+//   delay: 3.4,
+// });
+// TweenMax.staggerFrom(
+//   ".images > div",
+//   1, {
+//     y: "60",
+//     opacity: 0,
+//     ease: Power2.easeOut,
+//     delay: 6,
+//   },
+//   0.2
+// );
 
-TweenMax.from(".left", 3, {
-  left: "-50%",
-  ease: Expo.easeInOut,
-  delay: 3.4,
-});
-TweenMax.from(".header h1", 3, {
-  left: "-140%",
-  ease: Expo.easeInOut,
-  delay: 3.4,
-});
-TweenMax.staggerFrom(
-  ".images > div",
-  1,
-  {
-    y: "60",
+// TweenMax.staggerFrom(
+//   ".header > p",
+//   1, {
+//     y: "60",
+//     opacity: 0,
+//     ease: Power2.easeOut,
+//     delay: 5.6,
+//   },
+//   0.2
+// );
+
+var tl = new TimelineLite({
+  paused: true
+})
+
+var tlF = new TimelineLite({
+  paused: true,
+})
+
+var tlT1 = new TimelineLite({
+  paused: true,
+  delay: 3
+})
+
+var tlT2 = new TimelineLite({
+  paused: true,
+  delay: 6
+})
+
+var tlS1 = new TimelineLite({
+  paused: true,
+  delay: 6
+})
+
+var tlText1S2 = new TimelineLite({
+  paused: true,
+  delay: 3
+})
+
+var tlText2S2 = new TimelineLite({
+  paused: true,
+  delay: 4
+})
+
+var tlText2S2 = new TimelineLite({
+  paused: true,
+  delay: 5
+})
+
+var tlText3S2 = new TimelineLite({
+  paused: true,
+  delay: 6
+})
+
+tl.set("#phaeton", {
+    y: 90,
+    width: 1400,
     opacity: 0,
-    ease: Power2.easeOut,
-    delay: 6,
-  },
-  0.2
-);
+    ease: Expo.easeInOut
+  })
+  .to("#phaeton", 10, {
+    y: -10,
+    width: 1000,
+    opacity: 1
+  });
 
-TweenMax.staggerFrom(
-  ".header > p",
-  1,
-  {
-    y: "60",
+tlF.set("#fragment", {
+    x: 140,
     opacity: 0,
-    ease: Power2.easeOut,
-    delay: 5.6,
-  },
-  0.2
-);
+    ease: Expo.easeInOut
+  })
+  .to("#fragment", 12, {
+    x: -100,
+    opacity: 1
+  });
 
-TweenMax.from(".link", 1, {
-  opacity: 0,
-  ease: SteppedEase.config(1),
-  repeat: -1,
-  repeatDelay: 0.2,
-  delay: 7.8,
-});
+tlT1.set("#text1", {
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+  .to("#text1", 8, {
+    opacity: 1
+  });
+
+tlT2.set("#text2", {
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+  .to("#text2", 11, {
+    opacity: 1
+  });
+
+
+tlS1.set("#scene1", {
+    opacity: 1,
+    ease: Expo.easeInOut
+  })
+  .to("#scene1", 3, {
+    opacity: 0
+  });
+
+tlText1S2.set("#text1S2", {
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+  .to("#text1S2", 3, {
+    opacity: 1
+  });
+
+  tlText2S2.set("#text2S2", {
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+  .to("#text2S2", 3, {
+    opacity: 1
+  });
+
+  tlText3S2.set("#text3S2", {
+    opacity: 0,
+    ease: Expo.easeInOut
+  })
+  .to("#text3S2", 3, {
+    opacity: 1
+  });
+
+
+
+$("#button").on("click", function () {
+  tl.play();
+  tlF.play();
+  tlT1.play();
+  tlT2.play();
+})
+
+$("#suite").on("click", function () {
+  tlS1.play();
+})
+
+
+$("#phaeton").on("click", function () {
+  $('#scene1').delay(500).fadeOut(2000);
+  document.getElementById('scene1').style.display = 'none';
+  tlText1S2.play();
+  tlText2S2.play();
+  tlText3S2.play();
+})
+
+
+
+// $('#scene1').delay(5000).fadeOut(4000);
+
 
 //game.sceneManager.next()
