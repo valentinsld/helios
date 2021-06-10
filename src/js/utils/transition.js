7729ed242bfce04ab9c34de7ff648cd54d1c918f
Valentin S <43878560+valentinsld@users.noreply.github.com>
2021-06-10 11:53:13 +0200

    feat(Transition)

--- src/js/utils/transition.js
+++ src/js/utils/transition.js
@@ -1,31 +1,79 @@
+import gsap from 'gsap'
+
+const TEXTS = [
+  [
+    'Scene1 -> Scene 2',
+    'Jamais le soleil n’avait rayonné si proche de la Terre ...',
+    'Sans l’intervention de Zeus, ciel, terre et océans se seraient changés en flammes.'
+  ],
+  [
+    'Scene2 -> Scene 3',
+    'Jamais le soleil n’avait rayonné si proche de la Terre ...',
+    'Sans l’intervention de Zeus, ciel, terre et océans se seraient changés en flammes.'
+  ]
+  ,
+  [
+    'Scene3 -> Scene 4',
+    'Jamais le soleil n’avait rayonné si proche de la Terre ...',
+    'Sans l’intervention de Zeus, ciel, terre et océans se seraient changés en flammes.'
+  ]
+]
+
 class Transition {
   constructor () {
-    this.dom = document.querySelector('body')
-
-    this.initDiv()
+    this.el = document.querySelector('.transition')
+    this.elContainerText = this.el.querySelector('.transition__texts')
   }
 
-  initDiv () {
-    this.div = document.createElement('div')
-    this.div.classList.add('transition')
+  async fadeIn (textIndex, duration = 6000, time = 800) {
+    this.timeStartAnimation = new Date()
+    this.minDuration = duration
 
-    this.dom.append(this.div)
-  }
+    // add text
+    TEXTS[textIndex].forEach((text) => {
+      const paragraphe = document.createElement('p')
+      paragraphe.innerHTML = text
 
-  async fade (time = 800) {
-    this.div.style.transitionDuration = time + 'ms'
-    window.requestAnimationFrame(() => {
-      this.div.classList.add('-fade')
+      this.elContainerText.append(paragraphe)
     })
 
+    // animation
+    gsap.to(
+      this.el,
+      {
+        opacity: 1,
+        duration: time / 1000
+      }
+    )
 
     return new Promise(resolve => {
-      setTimeout(() => {
-        this.div.classList.remove('-fade')
-        
+      setTimeout(() => {      
         resolve(true);
       }, time + 300);
     });
+  }
+
+  async fadeOut (funcEnd, time = 500) {
+    // calculer delay before play animation
+    const nowDate = new Date()
+    const delay = Math.max(this.minDuration - (nowDate - this.timeStartAnimation), 0) / 1000
+    console.log(delay)
+
+
+    // animation
+    this.el.style.transitionDuration = ''
+    const tl = gsap.timeline()
+
+    tl.to(
+      this.el,
+      {
+        opacity: 0,
+        delay,
+        duration: time / 1000,
+        onComplete: funcEnd
+      }
+    )
+
   }
 }
 
